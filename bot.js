bc38608f63008e97a049b09f905b2d55b14b7881
Serhann <serhanpw@gmail.com>
2017-08-14 20:10:40 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
-const Discord = require("discord.js");
-const client = new Discord.Client();
-const prefix = '&';
+const Discord  = require('discord.js');
+const client   = new Discord.Client();
+const settings = require('./settings.json')
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
@@ -8,37 +8,37 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.author.bot) return;
-  if (msg.content === 'of') {
+  if (msg.content.toLowerCase().includes('of')) {
    	msg.reply('Of deme ah de!');
   }
-  if (msg.content === 'ah') {
+  if (msg.content.toLowerCase().includes('ah')) {
    	msg.reply('Ah deme oh de!');
   }
-  if (msg.content === 'oh') {
+  if (msg.content.toLowerCase().includes('oh')) {
    	msg.reply('Oh deme püf de!');
   }
-  if (msg.content === 'püf') {
+  if (msg.content.toLowerCase().includes('püf')) {
    	msg.reply('Püf deme of de!');
   }
-  if (msg.content === 'günaydın') {
+  if (msg.content.toLowerCase().includes('günaydın')) {
    	msg.reply('sana da günaydın');
   }
-  if (msg.content === 'herkese günaydın') {
+  if (msg.content.toLowerCase().includes('herkese günaydın')) {
    	msg.reply('günaydın :)');
   }
-  if (msg.content === 'iyi geceler') {
+  if (msg.content.toLowerCase().includes('iyi geceler')) {
    	msg.reply('sana da iyi geceler');
   }
-  if (msg.content === 'sa') {
+  if (msg.content.toLowerCase().includes('sa')) {
    	msg.reply('as');
   }
-  if (msg.content === 'iyi akşamlar') {
+  if (msg.content.toLowerCase().includes('iyi akşamlar')) {
    	msg.reply('sana da iyi akşamlar');
   }
-  if (msg.content === 'selamın aleyküm') {
+  if (msg.content.toLowerCase().includes('selamın aleyküm')) {
    	msg.reply('aleyküm selam');
   }
-  if (msg.content === 'güle güle') {
+  if (msg.content.toLowerCase().includes('güle güle')) {
    	msg.reply('sana da güle güle');
   }
 });
