1932aa7561003b858275389f452beb299252abc0
Serhann <serhanpw@gmail.com>
2018-01-07 12:20:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,39 +8,17 @@ client.on('ready', () => {
 
 client.on('message', msg => {
   if (msg.author.bot) return;
-  if (msg.content.toLowerCase().includes('of')) {
-   	msg.reply('Of deme ah de!');
-  }
-  if (msg.content.toLowerCase().includes('ah')) {
-   	msg.reply('Ah deme oh de!');
-  }
-  if (msg.content.toLowerCase().includes('oh')) {
-   	msg.reply('Oh deme püf de!');
-  }
-  if (msg.content.toLowerCase().includes('püf')) {
-   	msg.reply('Püf deme of de!');
-  }
-  if (msg.content.toLowerCase().includes('günaydın')) {
-   	msg.reply('sana da günaydın');
-  }
-  if (msg.content.toLowerCase().includes('herkese günaydın')) {
-   	msg.reply('günaydın :)');
-  }
-  if (msg.content.toLowerCase().includes('iyi geceler')) {
-   	msg.reply('sana da iyi geceler');
-  }
-  if (msg.content.toLowerCase().includes('sa')) {
-   	msg.reply('as');
-  }
-  if (msg.content.toLowerCase().includes('iyi akşamlar')) {
-   	msg.reply('sana da iyi akşamlar');
-  }
-  if (msg.content.toLowerCase().includes('selamın aleyküm')) {
-   	msg.reply('aleyküm selam');
-  }
-  if (msg.content.toLowerCase().includes('güle güle')) {
-   	msg.reply('sana da güle güle');
-  }
+  if (msg.content.toLowerCase().includes('of')) msg.reply('Of deme ah de!');
+  if (msg.content.toLowerCase().includes('ah')) msg.reply('Ah deme oh de!');
+  if (msg.content.toLowerCase().includes('oh')) msg.reply('Oh deme püf de!');
+  if (msg.content.toLowerCase().includes('püf')) msg.reply('Püf deme of de!');
+  if (msg.content.toLowerCase().includes('günaydın')) msg.reply('sana da günaydın');
+  if (msg.content.toLowerCase().includes('herkese günaydın')) msg.reply('günaydın :)');
+  if (msg.content.toLowerCase().includes('iyi geceler')) msg.reply('sana da iyi geceler');
+  if (msg.content.toLowerCase().includes('sa')) msg.reply('as');
+  if (msg.content.toLowerCase().includes('iyi akşamlar')) msg.reply('sana da iyi akşamlar');
+  if (msg.content.toLowerCase().includes('selamın aleyküm')) msg.reply('aleyküm selam');
+  if (msg.content.toLowerCase().includes('güle güle')) msg.reply('sana da güle güle');
 });
 
 client.login(settings.token);
